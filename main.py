79846015320c88c5cd86890729d39a2e81bc4f25
yashashwinipai <144199774+yashashwinipai@users.noreply.github.com>
2023-12-16 13:25:30 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, Response
+import cv2
+import numpy as np
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array
+
+app = Flask(__name__)
+
+face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+classifier = load_model('model.h5')
+
+emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
+
+def detect_emotion():
+    cap = cv2.VideoCapture(0)
+
+    try:
+        while True:
+            _, frame = cap.read()
+            labels = []
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_classifier.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
+                roi_gray = gray[y:y + h, x:x + w]
+                roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
+
+                if np.sum([roi_gray]) != 0:
+                    roi = roi_gray.astype('float') / 255.0
+                    roi = img_to_array(roi)
+                    roi = np.expand_dims(roi, axis=0)
+
+                    prediction = classifier.predict(roi)[0]
+                    label = emotion_labels[prediction.argmax()]
+                    label_position = (x, y)
+                    cv2.putText(frame, label, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                else:
+                    cv2.putText(frame, 'No Faces', (30, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+            # Encode the frame into JPEG format
+            _, buffer = cv2.imencode('.jpg', frame)
+
+            # Convert the frame to bytes
+            frame_bytes = buffer.tobytes()
+
+            # Yield the frame in byte format
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+    finally:
+        # Release the capture object when the loop exits
+        cap.release()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(detect_emotion(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
